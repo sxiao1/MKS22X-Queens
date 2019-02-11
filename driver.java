f15be81fbaae711871c5df41a196a0d51d08c5b6
sxiao1 <sxiao1@stuy.edu>
2019-02-10 23:29:54 -0500

    tested some sizes

--- driver.java
+++ driver.java
@@ -1,6 +1,19 @@
 public class driver{
   public static void main(String[] args){
-    QueenBoard newboard = new QueenBoard(3);
+    QueenBoard newboard = new QueenBoard(5);
+    System.out.println(newboard.solve());
     System.out.println(newboard);
+    QueenBoard newboard1 = new QueenBoard(1);
+    System.out.println(newboard1.solve());
+    System.out.println(newboard1);
+    QueenBoard newboard2 = new QueenBoard(2);
+    System.out.println(newboard2.solve());
+    System.out.println(newboard2);
+    QueenBoard newboard3 = new QueenBoard(3);
+    System.out.println(newboard3.solve());
+    System.out.println(newboard3);
+    QueenBoard newboard4 = new QueenBoard(7);
+    System.out.println(newboard4.solve());
+    System.out.println(newboard4);
   }
 }
