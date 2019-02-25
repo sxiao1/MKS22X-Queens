9ec050e64913407bfbcf9dacae519568c91047ef
sxiao1 <sxiao1@stuy.edu>
2019-02-25 10:11:27 -0500

    tested the test cases and works

--- QueenBoard.java
+++ QueenBoard.java
@@ -132,4 +132,27 @@ public class QueenBoard{
     }
     return count;
   }
+  //testcase must be a valid index of your input/output array
+public static void runTest(int i){
+  QueenBoard b;
+  int[]tests =   {1,2,3,4,5,8};
+  int[]answers = {1,0,0,2,10,92};
+  if(i >= 0 && i < tests.length ){
+    int size = tests[i];
+    int correct = answers[i];
+    b = new QueenBoard(size);
+    int ans  = b.countSolutions();
+
+    if(correct==ans){
+      System.out.println("PASS board size: "+tests[i]+" "+ans);
+    }else{
+      System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+    }
+  }
+}
+public static String checker(int num){
+  runTest(1);
+  return "";
+}
+
 }
--- driver.java
+++ driver.java
@@ -1,6 +1,6 @@
 public class driver{
   public static void main(String[] args){
-    QueenBoard newboard = new QueenBoard(5);
+    /*QueenBoard newboard = new QueenBoard(5);
     System.out.println(newboard.solve());
     System.out.println(newboard);
     QueenBoard newboard1 = new QueenBoard(1);
@@ -15,6 +15,7 @@ public class driver{
     QueenBoard newboard4 = new QueenBoard(7);
     System.out.println(newboard4.countSolutions());
     System.out.println(newboard4.solve());
-    System.out.println(newboard4);
+    System.out.println(newboard4); */
+    System.out.println(QueenBoard.checker(1));
   }
 }
